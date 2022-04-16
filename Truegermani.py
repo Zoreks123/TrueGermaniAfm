aa14dcc6e68bc5f89a1ea326e5632ae699845419
Zoreks123 <95155330+Zoreks123@users.noreply.github.com>
2022-04-17 00:19:36 +0600

    Create Truegermani.py

---
+++ Truegermani.py
@@ -0,0 +1,92 @@
+# meta developer: @zxc_dfr3ddy
+
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .. import loader, utils
+
+import logging
+import datetime
+import time
+
+from telethon import types
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class AFKMod(loader.Module):
+    """Предоставляет сообщение о том, что вы недоступны.Module by @zxc_dfr3ddy"""
+    strings = {"name": "AFK",
+               "gone": "<b>[Германи]Я стал в АФК</b>🇩🇪",
+               "back": "<b>[Ноут германи!]Я больше не АФК🇩🇪</b>",
+               "afk": "<b>[Германи] Я сейчас не в сети ( Был в сети {} назад)🇩🇪.</b>",
+               "afk_reason": "<b>[@zxc_dfr3ddy]Я сейчас не в сети ( Был в сети {} назад).\nПричина:</b> <i>{}</i>🇩🇪"}
+
+    async def client_ready(self, client, db):
+        self._db = db
+        self._me = await client.get_me()
+
+    async def afkcmd(self, message):
+        """.afk [причина]"""
+        if utils.get_args_raw(message):
+            self._db.set(__name__, "afk", utils.get_args_raw(message))
+        else:
+            self._db.set(__name__, "afk", True)
+        self._db.set(__name__, "gone", time.time())
+        self._db.set(__name__, "ratelimit", [])
+        await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
+        await utils.answer(message, self.strings("gone", message))
+
+    async def unafkcmd(self, message):
+        """Удаляет AFK статус"""
+        self._db.set(__name__, "afk", False)
+        self._db.set(__name__, "gone", None)
+        self._db.set(__name__, "ratelimit", [])
+        await self.allmodules.log("unafk")
+        await utils.answer(message, self.strings("back", message))
+
+    async def watcher(self, message):
+        if not isinstance(message, types.Message):
+            return
+        if message.mentioned or getattr(message.to_id, "user_id", None) == self._me.id:
+            afk_state = self.get_afk()
+            if not afk_state:
+                return
+            logger.debug("tagged!")
+            ratelimit = self._db.get(__name__, "ratelimit", [])
+            if utils.get_chat_id(message) in ratelimit:
+                return
+            else:
+                self._db.setdefault(__name__, {}).setdefault("ratelimit", []).append(utils.get_chat_id(message))
+                self._db.save()
+            user = await utils.get_user(message)
+            if user.is_self or user.bot or user.verified:
+                logger.debug("User is self, bot or verified.")
+                return
+            if self.get_afk() is False:
+                return
+            now = datetime.datetime.now().replace(microsecond=0)
+            gone = datetime.datetime.fromtimestamp(self._db.get(__name__, "gone")).replace(microsecond=0)
+            diff = now - gone
+            if afk_state is True:
+                ret = self.strings("afk", message).format(diff)
+            elif afk_state is not False:
+                ret = self.strings("afk_reason", message).format(diff, afk_state)
+            await utils.answer(message, ret, reply_to=message)
+
+    def get_afk(self):
+        return self._db.get(__name__, "afk", False)
